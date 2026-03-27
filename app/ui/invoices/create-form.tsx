60dcd884c87b7bb98b67a5cba0683ae1033d3893
Your Name <cuentauniagus@gmail.com>
2026-03-27 16:44:47 -0300

    Fix para deploy

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,12 +9,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice,State } from '@/app/lib/actions';
-import { create } from 'domain';
+import { createInvoice } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState : State= {message:null, errors: {} };
+  const initialState = { message: '', errors: {} };
 
   const [state, formAction ]= useActionState(createInvoice,initialState);
   
